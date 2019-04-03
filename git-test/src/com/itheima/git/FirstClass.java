71b53e37bed907addfd21d29f209a8538f95c0ca
Fan <609485623@qq.com>
2019-04-03 14:14:29 +0800

    把本地工程添加到git

---
+++ git-test/src/com/itheima/git/FirstClass.java
@@ -0,0 +1,13 @@
+package com.itheima.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+
+        System.out.println(" so hard");
+        System.out.println(" oh no");
+
+
+    }
+
+
+}
